aef349be79b6214a5b81d87325976e97be533c59
Jonathan Le Brun <42697488+icyfry@users.noreply.github.com>
2024-05-23 17:41:44 +0200

    set solidity 0.8.26

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
--- test/Rochers.t.sol
+++ test/Rochers.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.26;
 
 import {DeployRochers} from "../script/DeployRochers.s.sol";
 import {Rochers} from "../src/Rochers.sol";
